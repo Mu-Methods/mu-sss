f71719daa69a5004cd09edeb97d7c05dce10fffd
ReneeSauce <105013852+ReneeSauce@users.noreply.github.com>
2022-11-18 13:38:14 -0800

    added mu-shamir package to yarn lock.
    setting up the plug-in

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,34 @@
-export const name = 'my-plugin'
+const muShamir = require('mu-shamir')
+/*
+import * as db from 'ssb-db2'
+import * as ebt from 'ssb-ebt'
+import * as friends from 'ssb-friends'
+import * as device from 'ssb-device-address'
+import * as identities from 'ssb-identities'
+import * as peerInvites from 'ssb-peer-invites'
+import * as conn from 'ssb-conn'
+import * as scheduler from 'ssb-replication-scheduler'
+*/
+
+export const name = 'mu-sss'
 export const version = '0.0.1'
 
 export const manifest = {
-  helloWorld: 'sync'
-}
-export const init = () => {
-  return {
-    helloWorld: () => console.log('Hello World')
-  }
+  helloWorld: 'sync',
+  share: 'sync',
+  randomShare: 'sync',
+  recover: 'sync',
+  recoverFull: 'sync' 
 }
 
+type tPoint = [bigint, bigint]
+
+export const init = (api) => {  
+  return {
+    helloWorld: () => console.log('Hello World'),
+    share,
+    randomShare,
+    recover,
+    recoverFull
+  }
+}
